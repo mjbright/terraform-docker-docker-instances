f89433fafa4e000ac1b8eb01f72dc38572242eb9
Michael Bright <1880109+mjbright@users.noreply.github.com>
2025-03-06 06:57:31 +0000

    Added workaround to prevent unnecessary container re-creations

--- resources.tf
+++ resources.tf
@@ -10,19 +10,16 @@ resource docker_container server {
   for_each  = var.containers
   
   name      = each.key
-  image     = docker_image.image[ each.key ].name
   hostname  = "${ each.key }.${ var.domain_name }"
 
+  # Note: use image 'id' not 'name' to prevent container re-creation: (due to name vs. sha256 usage)
+  #       issue#426 https://github.com/kreuzwerker/terraform-provider-docker/issues/426
+  image     = docker_image.image[ each.key ].image_id
 
   # Shouldn't be necessary: workarounds to prevent unnecessary container re-creations
   #
   # - setting of bridge mode:
   network_mode = "bridge"
-  #
-  # - ignore replacement due to image name being replaced by sha256 id:
-  lifecycle { ignore_changes = [ image ] }
-  #
-  # NOTE: remove above lifecycle block to allow container recreation on image change
 
 
   networks_advanced {
