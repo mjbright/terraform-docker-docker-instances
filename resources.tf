2c3fd5cc89651544dd050baacd4c15f63f61752f
Michael Bright <1880109+mjbright@users.noreply.github.com>
2025-03-06 06:46:34 +0000

    Added workaround to prevent unnecessary container re-creations

--- resources.tf
+++ resources.tf
@@ -13,6 +13,18 @@ resource docker_container server {
   image     = docker_image.image[ each.key ].name
   hostname  = "${ each.key }.${ var.domain_name }"
 
+
+  # Shouldn't be necessary: workarounds to prevent unnecessary container re-creations
+  #
+  # - setting of bridge mode:
+  network_mode = "bridge"
+  #
+  # - ignore replacement due to image name being replaced by sha256 id:
+  lifecycle { ignore_changes = [ image ] }
+  #
+  # NOTE: remove above lifecycle block to allow container recreation on image change
+
+
   networks_advanced {
     name    = var.network_bridge
   }
